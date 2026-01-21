42dc0dfb84059bf7fa597168db32a3f6a0a70f65
Alex Glowacki <22919693+AleGlowa@users.noreply.github.com>
2026-01-21 17:26:08 +0100

    general api request template

---
+++ nodes/Deapi/transport/index.ts
@@ -0,0 +1,54 @@
+import type {
+	IDataObject,
+	IExecuteFunctions,
+	IHttpRequestMethods,
+	ILoadOptionsFunctions,
+} from 'n8n-workflow';
+
+type RequestParameters = {
+	headers?: IDataObject;
+	body?: IDataObject | string;
+	qs?: IDataObject;
+	option?: IDataObject;
+};
+
+export async function apiRequest(
+  this: IExecuteFunctions | ILoadOptionsFunctions,
+  method: IHttpRequestMethods,
+  endpoint: string,
+  parameters?: RequestParameters,
+) {
+  const { body, qs, option, headers } = parameters ?? {};
+  // let headers = parameters?.headers ?? {};
+
+  // const credentials = await this.getCredentials('deApi');
+
+  // if (
+	// 	credentials.header &&
+	// 	typeof credentials.headerName === 'string' &&
+	// 	credentials.headerName &&
+	// 	typeof credentials.headerValue === 'string'
+	// ) {
+	// 	headers = {
+	// 		...headers,
+	// 		[credentials.headerName]: credentials.headerValue,
+	// 	};
+	// }
+
+  const url = `https://api.deapi.ai/api/v1/client${endpoint}`;
+
+  const options = {
+    headers,
+    method,
+    body,
+    qs,
+    url,
+    json: true,
+  };
+
+  if (option && Object.keys(option).length !== 0) {
+    Object.assign(options, option);
+  }
+
+  return await this.helpers.requestWithAuthentication.call(this, 'deApi', options);
+}
